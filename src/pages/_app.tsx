4f011e5e12e79e4c017ed81ab29cb6c9a43cc892
alessandroaw <alessandro.ariaw@gmail.com>
2024-01-27 16:03:41 +0700

    remove unused

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,14 @@
-import 'tailwindcss/tailwind.css'
-import '@/styles/styles.css'
-import { ThemeProvider } from 'next-themes'
-import Script from 'next/script'
+import "tailwindcss/tailwind.css";
+import "@/styles/styles.css";
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
-	return (
-		
-		<ThemeProvider attribute="class">
-			{/* <!-- Google tag (gtag.js) --> */}
-			<Script
-       				 src="https://www.googletagmanager.com/gtag/js?id=G-HERRG7L4LP"
-        				strategy="afterInteractive"
-      			/>
-      			<Script id="google-analytics" strategy="afterInteractive">
-				{`
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){window.dataLayer.push(arguments);}
-				gtag('js', new Date());
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
-				gtag('config', 'G-HERRG7L4LP');
-				`}
-			</Script>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	)
-}
-
-export default App
+export default App;
